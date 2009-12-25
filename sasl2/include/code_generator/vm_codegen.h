3e7b66b9529b20885f8868ff05dacff39cb69975
wuye9036@gmail.com <wuye9036@gmail.com>
2009-12-25 14:55:53 +0000

    rename some instructions add register allocator

--- sasl2/include/code_generator/vm_codegen.h
+++ sasl2/include/code_generator/vm_codegen.h
@@ -4,11 +4,17 @@
 #include "../vm/vm.h"
 #include "../syntax_tree/expression.h"
 #include "../syntax_tree/constant.h"
+#include <bitset>
+#include <cassert>
 
 class vm_codegen{
-	vector<instruction> ins_;
+	std::vector<instruction> ins_;
+	std::bitset<vm::register_count> reg_usage;
 
 public:
+	vm_codegen(): reg_usage(0){
+	}
+
 	vm_codegen& emit_expression( const binary_expression& expr ){
 		if ( expr.op != operators::add ){
 			return *this;
@@ -16,21 +22,76 @@ public:
 
 		int c0 = expr.left_expr->val;
 		int c1 = expr.right_expr->val;
-		ins_.push_back( instruction( op_loadrc, r0, c0 ) );
-		ins_.push_back( instruction( op_loadrc, r1, c1 ) );
-		ins_.push_back( instruction( op_add, r0, r1 ) );
+
+		vm::regid_t reg0 = allocate_reg();
+		vm::regid_t reg1 = allocate_reg();
+
+		_load_r_si( reg0, c0 );
+		_load_r_si( reg1, c1 );
+		_add_si( reg0, reg1 );
+		
+		free_reg(reg0);
+		free_reg(reg1);
+
 		return *this;
 	}
 
-	vm_codegen& emit_op( op_code op, int arg0 = 0, int arg1 = 0 ){
+	vm_codegen& emit_op( op_code op, vm::intreg_t arg0 = 0, vm::intreg_t arg1 = 0 ){
 		ins_.push_back( instruction(op, arg0, arg1) );
 		return *this;
 	}
-	const vector<instruction>& codes(){
+	const std::vector<instruction>& codes(){
 		return ins_;
 	}
 
+	//辅助函数
+	vm_codegen& reallocate_reg( vm::regid_t reg_id ){
+		assert( !reg_usage.test(reg_id) );
+		reg_usage.set( reg_id, true );
+		return *this;
+	}
 
+	vm::regid_t allocate_reg(){
+		for( vm::regid_t id = 0; id < vm::regid_t(vm::register_count); ++id ){
+			if ( !reg_usage.test(id) ){
+				reallocate_reg( id );
+				return id;
+			}
+		}
+		assert(false);
+		return -1;
+	}
+
+	vm_codegen& free_reg(vm::regid_t reg_id){
+		assert( reg_usage.test( reg_id ) );
+		reg_usage.set( reg_id, false );
+		return *this;
+	}
+
+	//指令生成
+	vm_codegen& _add_si( vm::regid_t dest, vm::regid_t src ){
+		emit_op( op_add_si, dest, src );
+		return *this;
+	}
+
+	vm_codegen& _load_r_si( vm::regid_t dest, vm::intreg_t c ){
+		emit_op( op_load_r_si, dest, c );
+		return *this;
+	}
+
+	// 将寄存器压到栈上，并释放寄存器占用
+	vm_codegen& _save_r( vm::regid_t reg_id ){
+		free_reg( reg_id );
+		emit_op( op_push, reg_id );
+		return *this;
+	}
+
+	// 将寄存器从栈上恢复，并设置寄存器被占用
+	vm_codegen& _restore_r( vm::regid_t reg_id ){
+		reallocate_reg( reg_id );
+		emit_op( op_pop, reg_id );
+		return *this;
+	}
 };
 
 #endif //SASL_CODE_GENERATOR_VM_CODEGEN_H
--- sasl2/include/vm/vm.h
+++ sasl2/include/vm/vm.h
@@ -3,20 +3,68 @@
 #include <vector>
 #include <iostream>
 
-using namespace std;
-
 typedef unsigned char byte;
 
 enum op_code{
+	/*********************************
+	*	辅助
+	*********************************/
 	op_nop = 0,
-	op_add,
-	op_push,
-	op_pop,
-	op_loadrc,
-	op_loadrs,	//从栈上读到内存中。第一个参数为寄存器号，第二个参数为栈基偏移
+	// 描述：		空指令
+	// 指令格式：	op_nop
+	// 参数：		无
 	op_halt,
+	// 描述：		在任意状态下，终止虚拟机的执行并停机
+	// 指令格式：	op_halt
+	// 参数：		无
+
+	/*********************************
+	*	整数代数运算
+	*********************************/
+	op_add_si,
+	// 描述：		将两个寄存器中的数以有符号整数的形式相加。
+	// 指令格式：	op_add_si REG0, REG1
+	// 参数：		REG0: 目标寄存器号，REG1: 源寄存器号
+
+	/*********************************
+	*	栈
+	*********************************/
+	op_push,
+	// 描述：		将寄存器压入栈中。
+	// 指令格式：	op_push REG0
+	// 参数：		REG0：要压入栈的寄存器号。
+	op_pop,
+	// 描述：		将栈中数据返回寄存器，并从栈中弹出相应数据。
+	// 指令格式：	op_pop REG0
+	// 参数：		REG0：接受返回值的寄存器号。
+
+	/*********************************
+	*	拷贝
+	*********************************/
+	op_load_r_si,
+	// 描述：		读入带符号常数到寄存器中
+	// 指令格式：	op_load_r_si REG0, CONST
+	// 参数：		REG0：目标寄存器号，CONST：有符号整数
+	op_load_r_ui,
+	// 描述：		读入无符号常数到寄存器中
+	// 指令格式：	op_load_r_ui REG0, CONST
+	// 参数：		REG0：目标寄存器号，CONST：无符号整数
+	op_load_r_ssi,
+	// 描述：		从栈中读取一个有符号数到寄存器中
+	// 指令格式：	op_load_r_ssi REG0, OFFSET
+	// 参数：		REG0：目标寄存器，CONST：栈基偏移地址
+
+	/*********************************
+	*	子过程调用
+	*********************************/	
 	op_call,
+	// 描述：		调用指令。依次将ebp和eip压栈，并跳转至目标地址。
+	// 指令格式：	op_call ADDR
+	// 参数：		ADDR：指令地址
 	op_ret
+	// 描述：		调用返回指令。清理子过程栈的使用，恢复父过程的栈基指针，并跳转会父指令的位置。
+	// 指令格式：	op_ret
+	// 参数：		无
 };
 
 enum reg{
@@ -40,6 +88,14 @@ struct instruction{
 class vm
 {
 public:
+	typedef intptr_t intreg_t;
+	typedef uintptr_t uintreg_t;
+	typedef intptr_t regid_t;
+	typedef intptr_t addr_t;
+	typedef intptr_t offset_t;
+
+	static const int register_count = 16;
+
 	vm(void);
 	~vm(void);
 
@@ -48,7 +104,7 @@ public:
 	// 将清空指令槽，eip置为0后开始调用，至op_halt退出。
 	// 返回r[0]的值。
 	********************************************************/
-	intptr_t raw_call(const vector<instruction>& ins){
+	intptr_t raw_call(const std::vector<instruction>& ins){
 		op_buffer = ins;
 		eip = 0;
 		ebp = 0;
@@ -87,7 +143,7 @@ public:
 			return false;
 		case op_nop:
 			break;
-		case op_add:
+		case op_add_si:
 			r[arg0] += r[arg1];
 			break;
 		case op_push: 
@@ -98,18 +154,18 @@ public:
 			}
 		case op_pop:
 			{
-				size_t pop_size = r[arg0];
-				stack_pop( pop_size );
+				stack_back_value( r[arg0] );
+				stack_pop( sizeof(r[arg0]) );
 				break;
 			}
-		case op_loadrc:
+		case op_load_r_si:
 			{
 				intptr_t& reg( r[arg0] );
 				intptr_t val = arg1;
 				reg = val;
 				break;
 			}
-		case op_loadrs:
+		case op_load_r_ssi:
 			{
 				intptr_t& reg( r[arg0] );
 				ebp_based_value( reg, arg1 );
@@ -193,12 +249,12 @@ public:
 	}
 
 	//virtual machine storage
-	vector<byte> stack;
-	vector<instruction> op_buffer;
+	std::vector<byte> stack;
+	std::vector<instruction> op_buffer;
 
 	intptr_t eip;
 	intptr_t ebp;
 
-	intptr_t r[16];
+	intptr_t r[register_count];
 	intptr_t jump_to;
 };
