d08b49df20a16d7bcc12a95b9d917cb32bbb367b
wuye9036@gmail.com <wuye9036@gmail.com>
2009-12-26 04:36:34 +0000

    add class vm_stack

--- sasl2/include/vm/vm.h
+++ sasl2/include/vm/vm.h
@@ -1,71 +1,11 @@
-#pragma once
+#ifndef SASL_VM_VM_H
+#define SASL_VM_VM_H
 
+#include "op_code.h"
+#include "vm_stack.h"
+#include <eflib/include/platform.h>
 #include <vector>
 #include <iostream>
-
-typedef unsigned char byte;
-
-enum op_code{
-	/*********************************
-	*	辅助
-	*********************************/
-	op_nop = 0,
-	// 描述：		空指令
-	// 指令格式：	op_nop
-	// 参数：		无
-	op_halt,
-	// 描述：		在任意状态下，终止虚拟机的执行并停机
-	// 指令格式：	op_halt
-	// 参数：		无
-
-	/*********************************
-	*	整数代数运算
-	*********************************/
-	op_add_si,
-	// 描述：		将两个寄存器中的数以有符号整数的形式相加。
-	// 指令格式：	op_add_si REG0, REG1
-	// 参数：		REG0: 目标寄存器号，REG1: 源寄存器号
-
-	/*********************************
-	*	栈
-	*********************************/
-	op_push,
-	// 描述：		将寄存器压入栈中。
-	// 指令格式：	op_push REG0
-	// 参数：		REG0：要压入栈的寄存器号。
-	op_pop,
-	// 描述：		将栈中数据返回寄存器，并从栈中弹出相应数据。
-	// 指令格式：	op_pop REG0
-	// 参数：		REG0：接受返回值的寄存器号。
-
-	/*********************************
-	*	拷贝
-	*********************************/
-	op_load_r_si,
-	// 描述：		读入带符号常数到寄存器中
-	// 指令格式：	op_load_r_si REG0, CONST
-	// 参数：		REG0：目标寄存器号，CONST：有符号整数
-	op_load_r_ui,
-	// 描述：		读入无符号常数到寄存器中
-	// 指令格式：	op_load_r_ui REG0, CONST
-	// 参数：		REG0：目标寄存器号，CONST：无符号整数
-	op_load_r_ssi,
-	// 描述：		从栈中读取一个有符号数到寄存器中
-	// 指令格式：	op_load_r_ssi REG0, OFFSET
-	// 参数：		REG0：目标寄存器，CONST：栈基偏移地址
-
-	/*********************************
-	*	子过程调用
-	*********************************/	
-	op_call,
-	// 描述：		调用指令。依次将ebp和eip压栈，并跳转至目标地址。
-	// 指令格式：	op_call ADDR
-	// 参数：		ADDR：指令地址
-	op_ret
-	// 描述：		调用返回指令。清理子过程栈的使用，恢复父过程的栈基指针，并跳转会父指令的位置。
-	// 指令格式：	op_ret
-	// 参数：		无
-};
 
 enum reg{
 	r0 = 0,
@@ -136,64 +76,7 @@ public:
 		return execute_op( ins.op, ins.arg0, ins.arg1 );
 	}
 
-	bool execute_op(op_code op, intptr_t arg0, intptr_t arg1){
-		switch (op)
-		{
-		case op_halt:
-			return false;
-		case op_nop:
-			break;
-		case op_add_si:
-			r[arg0] += r[arg1];
-			break;
-		case op_push: 
-			{
-				intptr_t& pushed_reg( r[arg0] );
-				stack_push( pushed_reg );
-				break;
-			}
-		case op_pop:
-			{
-				stack_back_value( r[arg0] );
-				stack_pop( sizeof(r[arg0]) );
-				break;
-			}
-		case op_load_r_si:
-			{
-				intptr_t& reg( r[arg0] );
-				intptr_t val = arg1;
-				reg = val;
-				break;
-			}
-		case op_load_r_ssi:
-			{
-				intptr_t& reg( r[arg0] );
-				ebp_based_value( reg, arg1 );
-				break;
-			}
-		case op_call:
-			{
-				intptr_t tar_addr = arg0;
-				stack_push( esp() );
-				stack_push( eip );
-				ebp = esp();
-				jump_to = tar_addr;
-				break;
-			}
-		case op_ret:
-			{
-				esp(ebp);
-				stack_back_value( jump_to );
-				stack_pop( sizeof( jump_to ) );
-				stack_back_value( ebp );
-				stack_pop( sizeof ( ebp ) );
-				++jump_to;
-				break;
-			}
-		}
-
-		return true;
-	}
+	bool execute_op(op_code op, intptr_t arg0, intptr_t arg1);
 
 	//instruction operators
 	void jump(){
@@ -206,50 +89,8 @@ public:
 		jump_to = 0;
 	}
 
-	//stack operators
-	void* stack_pos( intptr_t offset ){
-		return (void*)( (byte*)&(stack[0]) + offset );
-	}
-
-	template <typename ValueT>
-	void stack_value(ValueT& v, intptr_t addr){
-		memcpy( &v, stack_pos( addr ), sizeof(ValueT) );
-	}
-
-	template <typename ValueT>
-	void ebp_based_value(ValueT& v, intptr_t offset){
-		stack_value( v, ebp + offset );
-	}
-
-	template <typename ValueT>
-	void esp_based_value(ValueT& v, intptr_t offset){
-		stack_value( v, esp() + offset );
-	}
-
-	template <typename ValueT>
-	void stack_back_value(ValueT& v){
-		esp_based_value( v, - ((intptr_t)sizeof(ValueT)) );
-	}
-
-	void stack_pop(size_t size){
-		esp( esp() - (intptr_t)size );
-	}
-
-	template<typename ValueT>
-	void stack_push( const ValueT& v ){
-		stack.insert( stack.end(), (byte*)&v, (byte*)&v + sizeof(v) );	
-	}
-
-	intptr_t esp(){
-		return (intptr_t) stack.size();
-	}
-
-	void esp( intptr_t addr ){
-		stack.resize(addr);
-	}
-
 	//virtual machine storage
-	std::vector<byte> stack;
+	vm_stack<addr_t> stack;
 	std::vector<instruction> op_buffer;
 
 	intptr_t eip;
@@ -258,3 +99,5 @@ public:
 	intptr_t r[register_count];
 	intptr_t jump_to;
 };
+
+#endif //SASL_VM_VM_H
--- sasl2/src/vm/vm.cpp
+++ sasl2/src/vm/vm.cpp
@@ -6,3 +6,53 @@ vm::vm(void): eip(0), jump_to(0), ebp(0){
 vm::~vm(void)
 {
 }
+
+bool vm::execute_op(op_code op, intptr_t arg0, intptr_t arg1){
+	switch (op)
+	{
+	case op_halt:
+		return false;
+	case op_nop:
+		break;
+	case op_add_si:
+		r[arg0] += r[arg1];
+		break;
+	case op_push: 
+		{
+			stack.push( r[arg0] );
+			break;
+		}
+	case op_pop:
+		{
+			stack.pop( r[arg0] );
+			break;
+		}
+	case op_load_r_si:
+		{
+			r[arg0] = arg1;
+			break;
+		}
+	case op_load_r_s:
+		{
+			r[arg0] = stack.value_frame_based<intreg_t>( arg1 );
+			break;
+		}
+	case op_call:
+		{
+			intptr_t tar_addr = arg0;
+			stack.push( eip );
+			stack.enter_frame();
+			jump_to = tar_addr;
+			break;
+		}
+	case op_ret:
+		{
+			stack.leave_frame();
+			stack.pop( jump_to );
+			++jump_to;
+			break;
+		}
+	}
+
+	return true;
+}
