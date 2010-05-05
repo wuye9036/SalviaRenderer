6e13e67997afabcd6f1dcaf72b5f2f1892a06a1c
wuye9036@gmail.com <wuye9036@gmail.com>
2010-05-05 06:04:26 +0000

    SASL:  add type_qualifiers enumerations  SASL CANNOT BE COMPILED.

---
+++ sasl/enums/type_qualifiers.cpp
@@ -0,0 +1,66 @@
+
+#include "./type_qualifiers.h" 
+#include <boost/unordered_map.hpp>
+
+using namespace boost;
+using namespace std;
+
+
+const type_qualifiers type_qualifiers::_uniform ( 2 );
+const type_qualifiers type_qualifiers::none ( 1 );
+
+ 
+struct enum_hasher: public std::unary_function< type_qualifiers, std::size_t> {
+	std::size_t operator()( type_qualifiers const& val) const{
+		return hash_value(val.val_);
+	}
+};
+
+struct dict_wrapper_type_qualifiers {
+private:
+	boost::unordered_map< type_qualifiers, std::string, enum_hasher > enum_to_name;
+	boost::unordered_map< std::string, type_qualifiers > name_to_enum;
+
+public:
+	dict_wrapper_type_qualifiers(){
+		enum_to_name.insert( std::make_pair( type_qualifiers::_uniform, "_uniform" ) );
+		enum_to_name.insert( std::make_pair( type_qualifiers::none, "none" ) );
+
+		name_to_enum.insert( std::make_pair( "_uniform", type_qualifiers::_uniform ) );
+		name_to_enum.insert( std::make_pair( "none", type_qualifiers::none ) );
+
+	}
+
+	std::string to_name( type_qualifiers const& val ){
+		boost::unordered_map< type_qualifiers, std::string >::const_iterator
+			find_result_it = enum_to_name.find(val);
+			
+		if ( find_result_it != enum_to_name.end() ){
+			return find_result_it->second;
+		}
+
+		return "__unknown_enum_val__";
+	}
+
+	type_qualifiers from_name( const std::string& name){
+		boost::unordered_map< std::string, type_qualifiers >::const_iterator
+			find_result_it = name_to_enum.find(name);
+			
+		if ( find_result_it != name_to_enum.end() ){
+			return find_result_it->second;
+		}
+
+		throw "unexcepted enumuration name!";
+	}
+};
+
+static dict_wrapper_type_qualifiers s_dict;
+
+std::string type_qualifiers::to_name( const type_qualifiers& enum_val){
+	return s_dict.to_name(enum_val);
+}
+
+type_qualifiers type_qualifiers::from_name( const std::string& name){
+	return s_dict.from_name(name);
+}
+
---
+++ sasl/enums/type_qualifiers.h
@@ -0,0 +1,33 @@
+
+#ifndef SASL_ENUMS_TYPE_QUALIFIERS_H
+#define SASL_ENUMS_TYPE_QUALIFIERS_H
+
+#include "../enums/enum_base.h" 
+
+struct type_qualifiers :
+	public enum_base< type_qualifiers, int >
+	, bitwise_op< type_qualifiers >, equal_op< type_qualifiers >, value_op< type_qualifiers, int >
+{
+	friend struct enum_hasher;
+private:
+	type_qualifiers( const storage_type& val ): base_type( val ){}
+	
+public:
+	type_qualifiers( const this_type& rhs )
+		:base_type(rhs.val_)
+	{}
+	
+	this_type& operator = ( const this_type& rhs){
+		val_ = rhs.val_;
+		return *this;
+	}
+
+	const static this_type _uniform;
+	const static this_type none;
+
+
+	static std::string to_name( const this_type& enum_val );
+	static this_type from_name( const std::string& name );
+
+};
+#endif
