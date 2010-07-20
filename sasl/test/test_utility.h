9fe742c4f5b6972e2c7aabb3e096a3f41df675db
wuye9036@gmail.com <wuye9036@gmail.com>
2010-07-20 14:41:32 +0000

    sasl:   updated.   CAN NOT BE COMPILED.

--- sasl/test/test_utility.h
+++ sasl/test/test_utility.h
@@ -3,6 +3,9 @@
 
 #include <sasl/enums/buildin_type_code.h>
 #include <boost/shared_ptr.hpp>
+#include <boost/utility.hpp>
+#include <boost/type_traits/is_base_of.hpp>
+#include <boost/preprocessor/cat.hpp>
 #include <string>
 
 namespace sasl{
@@ -17,6 +20,10 @@ namespace sasl{
 	}
 }
 
+
+#define SASL_ENABLE_IF_IS_BASE_OF( derived, base ) \
+	typename ::boost::enable_if< ::boost::is_base_of<derived, base> >::type* BOOST_PP_CAT(dummy, derived) = 0
+
 using ::sasl::common::token_attr;
 using ::sasl::syntax_tree::buildin_type;
 using ::sasl::semantic::symbol;
@@ -26,4 +33,33 @@ boost::shared_ptr<token_attr> new_token( const std::string& lit );
 boost::shared_ptr<buildin_type> new_buildin_type( boost::shared_ptr<token_attr> tok, buildin_type_code btc );
 
 template <typename SymbolInfoT> void extract_symbol_info( boost::shared_ptr<SymbolInfoT>& syminfo, boost::shared_ptr<symbol> sym );
+
+// make syntax tree node for constructing syntax tree easily.
+boost::shared_ptr<token_attr> make_node( const ::std::string& lit );
+
+boost::shared_ptr<buildin_type> make_node( const buildin_type_code btc );
+
+template <typename T> boost::shared_ptr<variable_declaration> make_node( 
+	boost::shared_ptr<T> typespec,
+	const ::std::string& ident,
+	SASL_ENABLE_IF_IS_BASE_OF( T, sasl::syntax_tree::type_specifier)
+	)
+{
+}
+
+template <typename T, typename U> boost::shared_ptr<binary_expression> make_node(
+	boost::shared_ptr<T> lexpr, operators op, boost::shared_ptr<U> rexpr,
+	SASL_ENABLE_IF_IS_BASE_OF( T, expression ),
+	SASL_ENABLE_IF_IS_BASE_OF( U, expression )
+	)
+{
+}
+
+template <typename T>
+boost::shared_ptr<declaration_statement> make_node(
+	boost::shared_ptr<T> decl,
+	SASL_ENABLE_IF_IS_BASE_OF(T, declaration)
+	)
+{
+}
 #endif
