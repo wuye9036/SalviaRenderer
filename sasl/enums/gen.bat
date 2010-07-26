a1ee4acf7b8effd760d63708040db04e4af7ef4b
wuye9036@gmail.com <wuye9036@gmail.com>
2010-07-26 10:36:50 +0000

    sasl:   update CMake of enums

--- sasl/enums/gen.bat
+++ sasl/enums/gen.bat
@@ -1,4 +1,4 @@
 @echo off
-enums_generator.py enum_gen_config.xml
-enums_generator.py compiler_infos_proj.xml
+python enums_generator.py enum_gen_config.xml
+python enums_generator.py compiler_infos_proj.xml
 pause
