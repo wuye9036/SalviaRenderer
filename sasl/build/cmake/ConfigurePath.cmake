e22363bea20663798e63713470458b18247eaed2
wuye9036@gmail.com <wuye9036@gmail.com>
2010-09-14 06:08:01 +0000

    sasl:   x64 under msvc10 supported.   re-factory cmake file.

---
+++ sasl/build/cmake/ConfigurePath.cmake
@@ -0,0 +1,12 @@
+set ( softart_home_dir "${CMAKE_HOME_DIRECTORY}/../softart/" )
+set ( sasl_home_dir	"${CMAKE_HOME_DIRECTORY}/../" )
+
+################ configurate 3rd party library paths ###############
+set( 3rd_party_path "${softart_home_dir}/3rd_party/" )
+	
+################ set llvm path ################
+set( 3rd_party_llvm_path "${3rd_party_path}/llvm/" )
+set( 3rd_party_llvm_include_path
+	"${3rd_party_llvm_path}/include/" "${3rd_party_llvm_path}/config/${SASL_ENV_WITHOUT_BUILD_TYPE}/"
+	)
+set( 3rd_party_llvm_lib_path "${3rd_party_llvm_path}/lib/${SASL_ENVIRONMENT_NAME}/" )
