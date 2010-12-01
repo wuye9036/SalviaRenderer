effd9a613ec0567cf965b54cb97ed03c5ee13e83
wuye9036@gmail.com <wuye9036@gmail.com>
2010-12-01 16:53:40 +0000

    SASL

--- sasl/enums/codegen_depend.py
+++ sasl/enums/codegen_depend.py
@@ -1,31 +1,60 @@
-import sys, os
-
-targetTag = "Target"
-dependTag = "Depend"
-sigAttrname = "Signature"
-
-class DependGraphNode:
-	def __init__( self, args ):
-		pass
+import sys, os, md5, shutil
 		
-	def getName( self ):
-		pass
-		
-	def isLeaf( self ):
-		pass
-		
-class DependGraph:
+class FileChangement:
 	def __init__( self ):
-		pass
-
-	def Update( self ):
-		pass
+		self.signatures = {}
+		Load()
 		
-class FileDG:
-	def __loadSig( self, fileName ):
-		f = open( fileName )
-		if not f:
-			self.dg = None
-			return
+	def __ComputeHash( self, fileName ):
+		f = open( fileName, 'r' )
+		siger = md5.new()
+		for line in f.readlines():
+			siger.update(line)
+		f.close()
+		return siger.hexdigest()
+	
+	def IsAnyChanged( self, fileNames, isUpdateSigs ):
+		return any( [ self.__IsFileChanged(fName, isUpdateSigs) for fName in fileNames ] )
+	
+	def __IsFileChanged( self, fileName, isUpdateSigs ):
+		isChanged = False
+		fileExisted = os.path.exists(fileName)
+		fileHash = None
+		if fileExisted:
+			fileHash = self.__ComputeHash(fileName)
+			
+		if (not fileName in signatures) and fileExisted:
+			isChanged = True
+		elif (fileName in signatures) and (not fileExisted):
+			isChanged = True
+		elif self.signatures[fileName] != fileHash:
+			isChanged = True
+		
+		if isUpdateSigs and isChanged:
+			if fileHash == None:
+				del self.signatures[fileName]
+			self.signatures[fileName] = fileHash
+		
+		return isChanged
+		
+	def Load( self ):
+		f = open( ".md5", "r" )
+		if f:
+			signatures = dict( [ ( line[33:].strip(), line[0:31] ) for line in f.readlines() ] )
 		else:
-			# Load sig file
+			signatures = {}
+		
+	def Save( self ):
+		f = open( ".md5", "w" )
+		for sig in signature:
+			f.writeline( sig[1] + " " + sig[0] )
+		f.close()
+		
+	def UpdateFile( self, dest, source, needUpdate ):
+		destHash = self.__ComputeHash(dest)
+		sourceHash = self.__ComputeHash(src)
+		if destHash == sourceHash :
+			return False
+		if needUpdate:
+			shutil.copyfile( dest, source )
+			signatures[dest] = sourceHash
