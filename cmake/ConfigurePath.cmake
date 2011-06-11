a8e7d5486ab34adf0a6dc0a6b47d33db48162e60
wuye9036@gmail.com <wuye9036@gmail.com>
2011-06-11 09:05:51 +0800

    SALVIA:   Rename SOFTART_* in cmake to SALVIA_*

--- cmake/ConfigurePath.cmake
+++ cmake/ConfigurePath.cmake
@@ -1,15 +1,15 @@
-set ( SOFTART_HOME_DIR "${CMAKE_HOME_DIRECTORY}/../softart/" )
+set ( SALVIA_HOME_DIR "${CMAKE_HOME_DIRECTORY}/../softart/" )
 set ( SASL_HOME_DIR	"${CMAKE_HOME_DIRECTORY}/../" )
 
-set( SOFTART_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_HOME_DIRECTORY}/bin/${SOFTART_PLATFORM_NAME}" )
-set( SOFTART_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_HOME_DIRECTORY}/lib/${SOFTART_PLATFORM_NAME}" )
+set( SALVIA_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_HOME_DIRECTORY}/bin/${SALVIA_PLATFORM_NAME}" )
+set( SALVIA_LIBRARY_OUTPUT_DIRECTORY "${CMAKE_HOME_DIRECTORY}/lib/${SALVIA_PLATFORM_NAME}" )
 
 ################ configurate 3rd party library paths ###############
-set( 3rd_party_path "${SOFTART_HOME_DIR}/../3rd_party/" )
+set( 3rd_party_path "${SALVIA_HOME_DIR}/../3rd_party/" )
 	
 ################ set llvm path ################
 set( 3rd_party_llvm_path "${3rd_party_path}/llvm/" )
 set( 3rd_party_llvm_include_path
-	"${3rd_party_llvm_path}/include/" "${3rd_party_llvm_path}/config/${SOFTART_ENV_WITHOUT_BUILD_TYPE}/"
+	"${3rd_party_llvm_path}/include/" "${3rd_party_llvm_path}/config/${SALVIA_ENV_WITHOUT_BUILD_TYPE}/"
 	)
-set( 3rd_party_llvm_lib_path "${3rd_party_llvm_path}/lib/${SOFTART_ENVIRONMENT_NAME}" )
+set( 3rd_party_llvm_lib_path "${3rd_party_llvm_path}/lib/${SALVIA_ENVIRONMENT_NAME}" )
