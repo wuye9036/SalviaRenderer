da2de813af431eb151997f31b22e09ccdcb0b8c7
wuye9036@gmail.com <wuye9036@gmail.com>
2011-07-19 00:51:23 +0800

    SALVIA:   Update code lines.

--- toolkit/toy/update_codelines.bat
+++ toolkit/toy/update_codelines.bat
@@ -3,6 +3,7 @@
 @"./code_statistic.py" ../../eflib .h;.cpp;.py;.xgt;.xgp;.txt;.cmake > codelines_of_salvia.txt
 @"./code_statistic.py" ../../salviar .h;.cpp;.py;.xgt;.xgp;.txt;.cmake >> codelines_of_salvia.txt
 @"./code_statistic.py" ../../salviax .h;.cpp;.py;.xgt;.xgp;.txt;.cmake >> codelines_of_salvia.txt
+@"./code_statistic.py" ../../salviau .h;.cpp;.py;.xgt;.xgp;.txt;.cmake >> codelines_of_salvia.txt
 @"./code_statistic.py" ../../samples .h;.cpp;.py;.xgt;.xgp;.txt;.cmake >> codelines_of_salvia.txt
 @"./code_statistic.py" ../../sasl .h;.cpp;.py;.xgt;.xgp;.txt;.cmake >> codelines_of_salvia.txt
 
