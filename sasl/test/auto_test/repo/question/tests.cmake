e0791b14245d32a802a59674992c790856732e8b
wuye9036@gmail.com <wuye9036@gmail.com>
2011-11-18 16:31:32 +0800

    Update issue 122 SASL:   Now auto test repo are included into test case project.

---
+++ sasl/test/auto_test/repo/question/tests.cmake
@@ -0,0 +1,25 @@
+set( SALVIA_AUTO_TEST_HOME "${SASL_HOME_DIR}/sasl/test/auto_test/repo/question" )
+
+set( V1A1_TESTS
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/comments.ss"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/decl.ss"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/null.ss"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/preprocessors.ss"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/semantic.svs"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/semantic_fn.svs"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/semfn_par.svs"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/struct_semin.svs"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/vec_and_mat.svs"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/arithmetic.svs"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/function.ss"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/intrinsics.ss"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/intrinsics.svs"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/branches.ss"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/empty.ss"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/bool.ss"
+	"${SALVIA_AUTO_TEST_HOME}/v1a1/unary_operators.ss"
+)
+
+SOURCE_GROUP( "Tests\\v1a1\\General" REGULAR_EXPRESSION ".*\\.ss" )
+SOURCE_GROUP( "Tests\\v1a1\\Vertex Shaders" REGULAR_EXPRESSION ".*\\.svs" )
+SOURCE_GROUP( "Tests\\v1a1\\Pixel Shaders" REGULAR_EXPRESSION ".*\\.sps" )
