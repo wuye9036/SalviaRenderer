10938126334179f44060fe2fa2b76ca724c53d4d
wuye9036@gmail.com <wuye9036@gmail.com>
2011-12-14 11:18:47 +0800

    Update issue 142 SASL:   Add correct dependency to test repos.   Now test repos will be update correctly.

--- sasl/test/auto_test/src/copy_tests.py
+++ sasl/test/auto_test/src/copy_tests.py
@@ -1,7 +1,7 @@
 import os, sys, shutil
 
 if __name__ == "__main__":
-	if len(sys.argv) != 3:
-		sys.exit(1)
-	shutil.rmtree( sys.argv[2], True )
-	shutil.copytree( sys.argv[1], sys.argv[2] )
+	if( os.path.exists(sys.argv[2]) ):
+		shutil.rmtree( sys.argv[2] )
+	if( not os.path.exists(sys.argv[2]) ):
+		shutil.copytree( sys.argv[1], sys.argv[2] )
