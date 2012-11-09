4e76976f8bdaa06158ccd1bc1ec09d8450da92c7
wuye9036@gmail.com <wuye9036@gmail.com>
2012-11-09 19:08:52 +0800

    Update clean build script.

--- clean_build.py
+++ clean_build.py
@@ -6,14 +6,15 @@ class build_properties:
 		pass
 		
 if __name__ == "__main__":
-	props = build_properties( {
-			'boost_root' 	: 'D:/Programming/boost_1_48_0',
-			'build_root' 	: '.\\build',
-			'install_root'	: './',
+	props_ntx86_msvc10_debug = build_properties( {
+			'boost_root' 	: 'E:/Programming/Library/boost_1_44_0',
+			'build_root' 	: '../salvia_build',
+			'install_root'	: '../salvia_ntx86_msvc10/',
 			'arch'			: 'x86',
 			'toolset'		: 'msvc-10.0',
 			'config'		: 'Debug',
 			'cmake'			: 'cmake'
 		} )
-	build_all.build_all(props)
+	#Clean target first
+	build_all.build_all(props_ntx86_msvc10_debug)
 	pass
