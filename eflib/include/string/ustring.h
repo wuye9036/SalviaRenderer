aab601a6db164bc0ba3e4e06fc96ad168ec97a43
wuye9036@gmail.com <wuye9036@gmail.com>
2012-12-15 13:23:51 +0800

    Update issue 206 SASL:   Add the first version of fixed_basic_string which is optmizated for read-only.

--- eflib/include/string/ustring.h
+++ eflib/include/string/ustring.h
@@ -3,77 +3,95 @@
 
 #include <eflib/include/platform/config.h>
 #include <eflib/include/memory/atomic.h>
+
 #include <eflib/include/platform/boost_begin.h>
 #include <boost/functional/hash.hpp>
 #include <boost/shared_ptr.hpp>
 #include <eflib/include/platform/boost_end.h>
+
 #include <string>
 
 namespace eflib{
 
-template <typename CharT> struct ustring_data{
-	ustring_data( std::basic_string<CharT> const & data )
-		: data(data)
-	{
-		hash_code = boost::hash_value(data);
-	}
-	ustring_data(CharT const * data )
-		: data(data)
-	{
-		hash_code = boost::hash_value(data);
-	}
-	std::basic_string<CharT> data;
-	size_t hash_code;
-};
+	template <typename CharT>
+	class fixed_basic_string{
+	private:
+		typedef std::basic_string<
+			CharT, std::char_traits<CharT>, std::allocator<CharT>
+		> string_type;
 
-template <typename CharT>
-class basic_ustring{
+	public:
+		typedef typename string_type::iterator			iterator;
+		typedef typename string_type::const_iterator	const_iterator;
+		typedef typename string_type::size_type			size_type;
+		typedef typename string_type::difference_type	difference_type;
+		typedef typename string_type::pointer			pointer;
+		typedef typename string_type::const_pointer		const_pointer;
+		typedef typename string_type::reference			reference;
+		typedef typename string_type::const_reference	const_reference;
+		typedef typename string_type::value_type		value_type;
 
-public:
-	basic_string(){}
+	private:
+		class content_data
+		{
+			content_data(std::string const& content)
+				: is_hashed(0), hash_code(0), content(content)
+				// , begin( content.begin() ), end( content.end() )
+			{
+			}
 
-	basic_ustring( CharT const * val ){
-		pdata = new ustring_data(val);
-	}
+			atomic<int32_t>				is_hashed;
+			size_t						hash_code;
+			string_type					content;
+			/*
+			const_iterator				begin;
+			const_iterator				end;
+			*/
+		};
 
-	basic_ustring( std::basic_string<CharT> const& val ){
-		pdata.reset( new ustring_data(val) );
-	}
+		boost::shared_ptr<content_data> data_;
 
-	basic_ustring( basic_ustring<CharT> const& val ){
-		pdata = val.pdata;
-	}
-
-	~basic_ustring(){}
-
-	CharT const * c_str() const{
-		if( pdata ){
-			return pdata->data.c_str();
+	public:
+		fixed_basic_string(std::string const& content)
+			: data_(content)
+		{
 		}
-		return NULL;
-	}
+		
+		fixed_basic_string(const fixed_basic_string& rhs)
+			: data_(rhs.data_)
+		{
+		}
 
-	basic_ustring& operator = ( basic_ustring<CharT> const& val ){
-		pdata = val.pdata;
-		return *this;
-	}
+		fixed_basic_string& operator = (const fixed_basic_string& rhs)
+		{
+			data_ = rhs.data_;
+		}
 
-	bool operator == ( basic_ustring<CharT> const& data ){
-		return
-			pdata == data.pdata
-			|| pdata->hash_code == data.pdata->hash_code
-			|| pdata->data == data.pdata->data
-			;
-	}
+		const_iterator begin() const
+		{
+			return data_->content.begin();
+		}
 
-	bool operator != ( basic_ustring<CharT> const& data ){
-		return !( *this == data );
-	}
+		const_iterator end() const
+		{
+			return data_->content.end();
+		}
 
-private:
-	shared_ptr<ustring_data> pdata;
-};
+		std::string const& raw_string() const
+		{
+			return data_->content;
+		}
 
+		size_t compute_hash() const
+		{
+			if(!data_->is_hashed)
+			{	
+				data_->hash_code = boost::hash_value(data_->content);
+				data_->is_hashed = 1;
+			}
+			return data_->hash_code;
+		}
+	};
 }
 
 #endif
