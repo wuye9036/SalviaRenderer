8588cc5eb93e8cfe2b4434478bee3e88951dd821
wuye9036@gmail.com <wuye9036@gmail.com>
2013-06-06 10:56:16 +0800

    Update issue 235 SALVIA:   Add geom_setup_engine files to project but not be used yet.

--- salviar/include/geom_setup_engine.h
+++ salviar/include/geom_setup_engine.h
@@ -2,34 +2,73 @@
 
 #include <salviar/include/salviar_forward.h>
 
+#include <salviar/include/enums.h>
+
+#include <eflib/include/memory/pool.h>
+
+#include <eflib/include/platform/boost_begin.h>
+#include <boost/shared_array.hpp>
+#include <eflib/include/platform/boost_end.h>
+
 BEGIN_NS_SALVIAR();
+
+struct vs_output_op;
+class  vs_output;
+class  vertex_cache;
+struct viewport;
+struct thread_context;
 
 struct geom_setup_context
 {
 	vs_output_op const*	vso_ops;
 	vertex_cache*		dvc;
-	viewport const*		vp;
+	prim_type			prim;
+	size_t				prim_size;
+	size_t				prim_count;
+	bool				(*cull)(float area);
 };
 
+/*
+	Primitive Verts -> Primitive Packages -> Clipped Packages (Sparse) -> Compacted Clipped Primitive Verts
+*/
 class geom_setup_engine
 {	
 public:
-	void update(geom_setup_context*);
-	
-	vs_output** get_geometries();
+	void execute(geom_setup_context const*);
+
+	size_t verts_count() const
+	{
+		return static_cast<size_t>( clipped_package_compacted_addresses_[clipping_package_count_] );
+	}
+
+	vs_output** verts() const
+	{
+		return compacted_verts_.get();
+	}
 	
 private:
+	geom_setup_engine();
+
+	typedef eflib::pool::preserved_pool<vs_output> vs_output_pool;
+
 	void clip_geometries();
 	void compact_geometries();
 	
-	void threaded_clip_geometries();
-	void threaded_compact_geometries();
+	void threaded_clip_geometries(thread_context const* thread_ctx);
+	void threaded_compact_geometries(thread_context const* thread_ctx);
 	
 	boost::shared_array<vs_output_pool>		vso_pools_;
 	boost::shared_array<vs_output*>			clipped_verts_;
-	boost::shared_array<uint32_t>			package_clipped_verts_count_;
+	boost::shared_array<uint32_t>			clipped_package_verts_count_;
 	
-	geom_setup_context						ctxt_;
+	boost::shared_array<vs_output*>			compacted_verts_;
+
+	boost::shared_array<uint32_t>			clipped_package_compacted_addresses_;
+	int32_t									clipping_package_count_;
+
+	size_t									thread_count_;
+	
+	geom_setup_context const*				ctxt_;
 };
 
 END_NS_SALVIAR();
--- salviar/include/thread_context.h
+++ salviar/include/thread_context.h
@@ -2,13 +2,123 @@
 
 #include <salviar/include/salviar_forward.h>
 
+#include <eflib/include/platform/typedefs.h>
+
+#include <eflib/include/platform/boost_begin.h>
+#include <boost/atomic/atomic.hpp>
+#include <eflib/include/platform/boost_end.h>
+
 BEGIN_NS_SALVIAR();
 
 struct thread_context
 {
+	class package_cursor
+	{
+	private:
+		friend struct thread_context;
+
+		thread_context const*	owner;
+		int32_t					cur;
+
+		package_cursor(thread_context const* owner, int32_t cur)
+			: owner(owner), cur(cur)
+		{
+		}
+	public:
+		package_cursor(): owner(NULL), cur(0)
+		{
+		}
+
+		package_cursor(package_cursor const& rhs)
+			: owner(rhs.owner), cur(rhs.cur)
+		{
+		}
+
+		package_cursor& operator = (package_cursor const& rhs)
+		{
+			owner = rhs.owner;
+			cur = rhs.cur;
+			return *this;
+		}
+
+		int32_t package_index() const
+		{
+			return cur;
+		}
+
+		std::pair<int32_t, int32_t> item_range() const
+		{
+			int32_t beg = cur * owner->package_size;
+			int32_t end = std::min(beg + owner->package_size, owner->item_count);
+			return std::make_pair(beg, end);
+		}
+
+		bool valid() const
+		{
+			return cur < owner->package_count;
+		}
+	};
+
+	size_t					thread_id;
+
 	int32_t					item_count;
-	boost::atomic<int32_t>*	package_token;
+	boost::atomic<int32_t>*	working_package_id;
 	int32_t					package_size;
+	int32_t					package_count;
+
+	static int32_t compute_package_count(int32_t item_count, int32_t package_size)
+	{
+		return (item_count + package_size - 1) / package_size;
+	}
+
+	package_cursor next_package() const
+	{
+		return package_cursor(this, (*working_package_id)++);
+	}
 };
+
+inline void init_thread_context(
+	thread_context* ctxts, size_t N,
+	int32_t item_count, boost::atomic<int32_t>* working_package_id, int32_t package_size)
+{
+	for(size_t i = 0; i < N; ++i)
+	{
+		ctxts[i].thread_id			= i;
+		ctxts[i].item_count			= item_count;
+		ctxts[i].working_package_id = working_package_id;
+		ctxts[i].package_size		= package_size;
+		ctxts[i].package_count		= thread_context::compute_package_count(item_count, package_size);
+	}
+}
+
+template <typename ThreadFuncT> // ThreadFuncT = function <void (thread_context const*)>
+inline void execute_threads(ThreadFuncT const& fn, int32_t item_count, int32_t package_size, int32_t thread_count)
+{
+	// Compute package information
+	int32_t package_count = thread_context::compute_package_count(item_count, package_size);
+
+	boost::atomic<int32_t>				working_package;
+	boost::shared_array<thread_context>	thread_ctxts(new thread_context[thread_count]);
+
+	// Initialize contexts per thread
+	thread_context* pctxts = thread_ctxts.get();
+	init_thread_context(thread_ctxts.get(), thread_count, package_count, &working_package, package_size);
+
+	// Call functions by context.
+	for (size_t i = 0; i < thread_count - 1; ++ i)
+	{
+		global_thread_pool().schedule( [&fn, pctxts, i] () { fn(&pctxts[i]); } );
+	}
+	fn(&pctxts[thread_count-1]);
+
+	global_thread_pool().wait();
+}
+
+template <typename ThreadFuncT> // ThreadFuncT = function <void (thread_context const*)>
+inline void execute_threads(ThreadFuncT const& fn, int32_t item_count, int32_t package_size)
+{
+	int32_t thread_count = static_cast<int32_t>(eflib::num_available_threads());
+	execute_threads(fn, item_count, package_size, thread_count);
+}
 
 END_NS_SALVIAR();
