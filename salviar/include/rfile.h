d9c27c6eb0c89fb9300efd767dfab7dd4b3af0fa
wuye9036@gmail.com <wuye9036@gmail.com>
2014-06-11 09:12:40 -0700

    Update issue 258 rfile.h is updated.

--- salviar/include/rfile.h
+++ salviar/include/rfile.h
@@ -63,8 +63,30 @@ enum class reg_types
 
 struct buffer_uid
 {
+	buffer_uid()
+		:slot(reg_slots::unknown), index(0)
+	{
+	}
+	
+	buffer_uid(reg_slots slot, uint32_t index)
+		:slot(slot), index(index);
+	{
+	}
+	
+	buffer_uid(buffer_uid const& rhs)
+		:slot(rhs.slot), index(rhs.index)
+	{
+	}
+	
+	buffer_uid& operator = (buffer_uid const& rhs)
+	{
+		slot = rhs.slot;
+		index = rhs.index;
+		return *this;
+	}
+	
 	reg_slots slot;
-	size_t	  buf_index;
+	size_t	  index;
 };
 
 struct reg_name
@@ -73,20 +95,28 @@ struct reg_name
 	uint32_t	reg_index;
 	uint32_t	elem;
 	
-	reg_name(reg_slots slot, uint32_t buf, uint32_t reg_index, uint32_t elem)
-		: slot(slot), buffer_index(buf), reg_index(reg_index), elem(elem)
+	reg_name(): reg_index(0), elem(0)
+	{
+	}
+	
+	reg_name(reg_slots slot, uint32_t buf_index, uint32_t reg_index, uint32_t elem)
+		: buf(slot, buf_index), reg_index(reg_index), elem(elem)
+	{
+	}
+	
+	reg_name(buffer_uid buf, uint32_t reg_index, uint32_t elem)
+		: buf(buf), reg_index(reg_index), elem(elem)
 	{
 	}
 	
 	reg_name(reg_name const& rhs)
-		: slot(rhs.slot), buffer_index(rhs.buffer_index), reg_index(rhs.reg_index), elem(rhs.elem)
+		: buf(rhs.buf), reg_index(rhs.reg_index), elem(rhs.elem)
 	{
 	}
 	
 	reg_name& operator = (reg_name const& rhs)
 	{
-		slot = rhs.slot;
-		buffer_index = rhs.buffer_index;
+		buf = rhs.buf;
 		reg_index = rhs.reg_index;
 		elem = rhs.elem;
 		return *this;
@@ -94,10 +124,10 @@ struct reg_name
 	
 	reg_name advance(size_t distance) const
 	{
-		return reg_name(slot, buffer_index, reg_index+distance, 0);
+		return reg_name(buf, reg_index+distance, 0);
 	}
 	
-	bool valid()
+	bool valid() const
 	{
 		return buf.slot == reg_slots::unknown;
 	}
@@ -106,6 +136,11 @@ struct reg_name
 struct buffer_regs
 {
 public:
+	buffer_regs(reg_slots slot, uint32_t index)
+		: uid_(slot, index), total_reg_count(0)
+	{
+	}
+	
 	alloc_result alloc_reg(size_t sz, reg_name const& rname)
 	{	
 		reg_name reg_end;
@@ -156,8 +191,14 @@ public:
 		}
 	}
 	
-	reg_name find_reg(semantic_value const& sem) const
+	reg_name find_reg(semantic_value const& sv) const
 	{
+		auto iter = sv_reg_.find(sv);
+		if(iter == sv_reg_.end())
+		{
+			return reg_name::invalid();
+		}
+		return iter->second;
 	}
 	
 	boost::icl::interval<reg_name>
@@ -168,61 +209,63 @@ public:
 		{
 			return boost::icl_interval<reg_name>();
 		}
-		
 		return iter->second;
 	}
 	
-	void update_packed_index()
+	void update_addr()
 	{
-		uint32_t total_reg_count = 0;
-		for(auto const& reg_interval: used_regs_)
+		total_reg_count_ = 0;
+		for(auto& range_addr: used_regs_)
 		{
-			uint32_t reg_count = reg_interval.up.reg_index - reg_interval.low.reg_index;
-			
+			auto& reg_range = range_addr.first;
+			uint32_t reg_count = reg_range.up.reg_index - reg_range.low.reg_index;
+			range_addr.second = total_reg_count_;
+			total_reg_count_ += reg_count;
 		}
+	}
+	
+	uint32_t used_reg_count() const
+	{
+		return total_reg_count_;
 	}
 	
 private:
 	alloc_result alloc_reg(reg_name& beg, reg_name& end, size_t sz)
 	{
-		auto& slot_used_regs = used_regs_[slot];
-		beg = slot_used_regs.empty() ? reg_name(uid_, 0, 0) : boost::icl::last(slot_used_regs).next();
+		auto& slot_used_regs = used_regs_;
+		beg = slot_used_regs.empty() ? reg_name(uid_, 0, 0) : boost::icl::last(slot_used_regs).first.next();
 		return alloc_reg(end, beg, sz);
 	}
 	
 	alloc_result alloc_reg(reg_name& end, reg_name const& beg, size_t sz)
 	{
 		end = reg_name.advance( align_up(sz, 16) / 16 );
-		slot_used_regs_.add( icl::right_open(beg, end) );
+		used_regs_.add( make_pair(icl::right_open(beg, end), 0) );
 		return alloc_result::ok;
 	}
 	
 	bool assign_variable(interval<reg_name> const& regs, std::string const& reg_name)
 	{
-		if( used_regs_[slot].intersects(reg_range) )
+		if( used_regs_.intersects(reg_range) )
 		{
 			return false;
 		}
 		
-		lut_var_reg_[vname] = reg_range;
+		var_reg_[vname] = reg_range;
 		return true;
 	}
 	
-	buffer_uid									uid_;
-	boost::icl::interval_set<reg_name>			used_regs_;
+	buffer_uid	uid_;
+	uint32_t	total_reg_count_;
 	
+	boost::icl::interval_map<reg_name, uint32_t /*physical_reg*/>
+														used_regs_;
 	std::vector< std::pair<std::string /*var name*/, uint32_t /*size*/> >
-												pending_vars_;
-												
-	// Lookup table
-	//		1. semantic - registers (1:N)	- X
-	//		2. register - semantic  (1:1)
-	//		3. variable - register  (1:1)
-	//		4. register - variable  (1:1)
-	std::map<reg_name, semantic_value>								reg_sv_;
-	std::map<reg_name, std::string>									reg_var_;
-	std::map<std::string, interval<reg_name>>						var_reg_;
-	std::map<uint32_t /*reg index*/, uint32_t /*physical index*/>	reg_packed_index_;
+														pending_vars_;
+	std::map<semantic_value, reg_name>					sv_reg_;
+	std::map<reg_name, semantic_value>					reg_sv_;
+	std::map<std::string, interval<reg_name>>			var_reg_;
+	std::map<uint32_t /*reg index*/, uint32_t /*addr*/>	reg_addr_;
 };
 
 class rfile_sasl_mapping
@@ -230,41 +273,89 @@ class rfile_sasl_mapping
 public:
 	void initialize(shader_profile prof);
 	
-	buffer_regs& buffer(reg_slots slot, size_t buf_index)
+	buffer_regs* buffer(reg_slots slot, uint32_t buf_index)
 	{
-	}
-	
-	buffer_regs& buffer(buffer_uid buf_uid)
-	{
-	}
-	
-	buffer_regs const& buffer(reg_slots slot, size_t buf_index) const
-	{
-	}
-	
-	buffer_regs const& buffer(buffer_uid buf_uid) const
-	{
-	}
-	
-	void update_variable_reg_usage()
-	{
-		for (uint32_t slot = static_cast<uint32_t>(reg_slots::unknown); slot < static_cast<uint32_t>(reg_slots::count); ++slot)
+		auto& slot_bufs = buffer_regs_[static_cast<uint32_t>(slot)];
+		
+		if( buf_index < slot_bufs.size() )
 		{
-			
+			if(!slot_bufs[buf_index])
+			{
+				slot_bufs[buf_index].reset(new buffer_regs(slot, buf_index));
+			}
+			return slot_bufs[buf_index].get();
+		}
+		else 
+		{
+			return nullptr;
 		}
 	}
 	
-	void update_reg_physical_pos();
-	
-	uint32_t physical_register_count(reg_slot slot);
-	
-	uint32_t physical_pos(reg_slot slot, reg_name const& rname)
+	buffer_regs* buffer(buffer_uid buf_uid)
 	{
+		return buffer(buf_uid.slot, buf_uid.index);
+	}
+	
+	buffer_regs const* buffer(reg_slots slot, uint32_t buf_index) const
+	{
+		auto& slot_bufs = buffer_regs_[static_cast<uint32_t>(slot)];
 		
+		if( buf_index < slot_bufs.size() )
+		{
+			return slot_bufs[buf_index].get();
+		}
+		else 
+		{
+			return nullptr;
+		}
+	}
+	
+	buffer_regs const* buffer(buffer_uid buf_uid) const
+	{
+		return buffer(buf_uid.slot, buf_uid.index);
+	}
+	
+	void update_reg_address()
+	{
+		for(uint32_t slot = static_cast<uint32_t>(reg_slots::unknown); slot = static_cast<uint32_t>(reg_slots::count); ++slot)
+		{
+			auto& slot_bufs		= buffer_regs_[slot];
+			auto& slot_buf_addr = buf_start_addr_[slot];
+			
+			uint32_t total = 0;
+			for(auto& buf: slot_bufs)
+			{
+				slot_buf_addr.push_back(total);
+				total += buf.used_reg_count();
+			}
+			used_reg_count_[slot] = total;
+		}
+	}
+	
+	uint32_t used_reg_count(reg_slot slot)
+	{
+		auto& slot_bufs = buffer_regs_[static_cast<uint32_t>(slot)];
+		uint32_t total = 0;
+		for(auto& buf: slot_bufs)
+		{
+			total += buf.used_regsiter_count()
+		}
+	}
+	
+	uint32_t reg_addr(reg_name const& rname)
+	{
+		uint32_t slot = static_cast<uint32_t>(rname.buf.slot);
+		return	buf_start_addr_[slot][rname.buf.index] +
+				buffer_regs_[slot][rname.buf.index]->reg_addr(rname);
 	}
 	
 private:
-	std::vector< boost::unique_ptr<buffer_regs> > buffer_regs_[static_cast<uint32_t>(reg_slots::count)];
+	std::vector< boost::unique_ptr<buffer_regs> >
+				buffer_regs_	[static_cast<uint32_t>(reg_slots::count)];
+	uint32_t	used_reg_count_	[static_cast<uint32_t>(reg_slots::count)];
+	std::vector<uint32_t>
+				buf_start_addr_	[static_cast<uint32_t>(reg_slots::count)];
+				
 };
 
 class rfile_hlsl_bc_mapping
