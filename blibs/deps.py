1fba0b2a107956b72cf8b6ccaeffbbae8b6b6d1d
Ye WU <wuye9036@gmail.com>
2015-06-28 23:39:21 -0700

    Update #1 Coding for deps downloading; Added download_list.py to repo.

--- blibs/deps.py
+++ blibs/deps.py
@@ -1,9 +1,10 @@
-﻿import env, urllib2, os
+﻿import env, urllib2, os, download_list, shutil
 
 def github_commit(commit):
     return "https://raw.githubusercontent.com/wuye9036/SalviaDeps/release/%s/" % commit
 
 CHECK_METHOD_HASH = "CM_HASH"
+
 CHECK_METHOD_TAGF = "CM_TAGF"
 
 RAW_FILE          = "RAW_FILE"
@@ -36,22 +37,9 @@ def gen_file_md5(file_name):
 COMMIT = ""
 
 DOWNLOAD_FILE_LIST = [
-    downaload_info(github_commit(COMMIT), "tools/linux/7z",      CM_HASH, RAW_FILE,          False, "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "tools/linux/7z.so",   CM_HASH, RAW_FILE,          False, "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "tools/win32/7z.exe",  CM_HASH, RAW_FILE,          False, "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "tools/win32/7z.dll",  CM_HASH, RAW_FILE,          False, "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "3rd_party/llvm",      CM_HASH, COMPRESSED_FOLDER, True,  "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "3rd_party/boost",     CM_HASH, COMPRESSED_FOLDER, True,  "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "3rd_party/freetype",  CM_HASH, COMPRESSED_FOLDER, True,  "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "3rd_party/freeimage", CM_HASH, COMPRESSED_FOLDER, True,  "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "resource/sponza_lq",  CM_HASH, COMPRESSED_FOLDER, True,  "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "resource/commom",     CM_HASH, COMPRESSED_FOLDER, True,  "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "resource/M134",       CM_HASH, COMPRESSED_FOLDER, True,  "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "resource/cup",        CM_HASH, COMPRESSED_FOLDER, True,  "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "resource/fonts",      CM_HASH, COMPRESSED_FOLDER, True,  "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "resource/morph",      CM_HASH, COMPRESSED_FOLDER, True,  "xxxxxxxxxxxxxxx"),
-    downaload_info(github_commit(COMMIT), "resource/astro_boy",  CM_HASH, COMPRESSED_FOLDER, True,  "xxxxxxxxxxxxxxx"),
-]
+    download_info(github_commit(COMMIT), res_path, CHECK_METHOD_HASH, res_type, not "7z" in res_path, tag)
+    for res_path, res_type, tag in download_list.DOWNLOAD_LIST
+    ]
 
 def download_file(url, rel_path):
     u = urllib2.urlopen(url)
@@ -83,8 +71,12 @@ def need_update(proj_root, dl_info):
     store_path = os.path.join(proj_root, "downloads", dl_info.store_rel_path)
     dist_path  = os.path.join(proj_root, dl_info.dist_rel_path)
 
-    if not os.path.exists(dist_path):
-        return True
+    if dl_info.res_type == COMPRESSED_FOLDER:
+        if not os.path.isdir(dist_path):
+            return True
+    else:
+        if not os.path.isfile(dist_path):
+            return True
 
     if not os.path.isfile(store_path):
         return True
@@ -94,12 +86,19 @@ def need_update(proj_root, dl_info):
 
     return False
 
-
 def download(proj_root, dl_info):
     pass
     
 def distribute(proj_root, dl_info):
-    pass
+    assert isinstance(dl_info, download_info)
+    if not dl_info.need_distribute: return
+    dist_parent = os.path.dirname(dl_info.dist_rel_path)
+    src_full_path = os.path.join(proj_root, "downloads", dl_info.store_rel_path)
+    dst_parent_full_path = os.path.join(proj_root, dist_parent)
+    if dl_info.res_type in [COMPRESSED_FILE, COMPRESSED_FOLDER]:
+        decompress(src_full_path, dst_parent_full_path)
+    else:
+        shutil.copy(src_full_path, dst_parent_full_path)
 
 def update(proj_root, dl_info):
     if need_update(proj_root, dl_info):
