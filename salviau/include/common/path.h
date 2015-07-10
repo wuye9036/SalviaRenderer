e312a0ffaf7766f25740bfb118e860727c5a77b3
Ye WU <wuye9036@gmail.com>
2015-07-09 17:55:41 -0700

    Update #4 find_path supports both narrow string and Unicode string.

--- salviau/include/common/path.h
+++ salviau/include/common/path.h
@@ -6,7 +6,10 @@
 
 BEGIN_NS_SALVIAU();
 
-std::_tstring SALVIAU_API find_file(std::_tstring const& relative_path);
-std::_tstring SALVIAU_API find_file(std::_tstring const& relative_path, std::vector<std::_tstring> const& candidates);
+
+std::string  SALVIAU_API find_path(std::string const& relative_path);
+std::string  SALVIAU_API find_path(std::string const& relative_path, std::vector<std::string> const& candidates);
+std::wstring SALVIAU_API find_path(std::wstring const& relative_path);
+std::wstring SALVIAU_API find_path(std::wstring const& relative_path, std::vector<std::wstring> const& candidates);
 
 END_NS_SALVIAU();
--- salviau/src/common/path.cpp
+++ salviau/src/common/path.cpp
@@ -6,41 +6,64 @@
 #include <boost/filesystem.hpp>
 #include <eflib/include/platform/boost_end.h>
 
-using std::_tstring;
 using std::vector;
+
+using std::string;
+using std::wstring;
+
+using boost::filesystem::wpath;
+using boost::filesystem::path;
 
 BEGIN_NS_SALVIAU();
 
-#if defined(EFLIB_UNICODE)
-	using _tpath = boost::filesystem::wpath;
-#else
-	using _tpath = boost::filesystem::path;
-#endif
-
-_tstring find_file(_tstring const& relative_path)
-{
-	vector<_tstring> candidate_folders
-	{
-		_EFLIB_T("./resources"),
-		_EFLIB_T("../resources"),
-		_EFLIB_T("../../resources"),
-		_EFLIB_T("../../../resources")
-	};
-
-	return find_file(relative_path, candidate_folders);
-}
-
-_tstring find_file(_tstring const& relative_path, vector<_tstring> const& candidates)
+template <typename StringT>
+StringT find_path_impl(StringT const& relative_path, vector<StringT> const& candidates)
 {
 	for (auto const& parent_folder: candidates)
 	{
-		auto full_path = _tpath(parent_folder) / _tpath(relative_path);
+		auto full_path = path(parent_folder) / path(relative_path);
 		if (boost::filesystem::exists(full_path))
 		{
-			return full_path.generic_string<_tstring>();
+			return full_path.generic_string<StringT>();
 		}
 	}
-	return _tstring();
+	return StringT();
+}
+
+string find_path(string const& relative_path)
+{
+	vector<string> candidate_folders
+	{
+		"./resources",
+		"../resources",
+		"../../resources",
+		"../../../resources"
+	};
+
+	return find_path_impl(relative_path, candidate_folders);
+}
+
+wstring find_path(wstring const& relative_path)
+{
+	vector<wstring> candidate_folders
+	{
+		L"./resources",
+		L"../resources",
+		L"../../resources",
+		L"../../../resources"
+	};
+
+	return find_path_impl(relative_path, candidate_folders);
+}
+
+string find_path(string const& relative_path, vector<string> const& candidates)
+{
+	return find_path_impl(relative_path, candidates);
+}
+
+wstring find_path(wstring const& relative_path, vector<wstring> const& candidates)
+{
+	return find_path_impl(relative_path, candidates);
 }
 
 END_NS_SALVIAU();
