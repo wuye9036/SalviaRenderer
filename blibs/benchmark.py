c9b3ea6a8e994a071ebb738503140f82a7c2c48b
Ye Wu <wuye9036@gmail.com>
2019-09-28 04:44:01 -0700

    Fixed path issue when lauching exe. Improved logs.

--- blibs/benchmark.py
+++ blibs/benchmark.py
@@ -4,13 +4,14 @@ import os
 import platform
 import subprocess
 
-from . import cpuinfo, util
+from . import cpuinfo
+from . import diagnostic
 
 
 class benchmark_runner:
-    def __init__(self, source_root_dir, install_dir, git_path):
+    def __init__(self, source_root_dir, binary_dir, git_path):
         self._root_dir = source_root_dir
-        self._install_dir = install_dir
+        self._binary_dir = binary_dir
         self._git_path = git_path
 
     @staticmethod
@@ -41,20 +42,23 @@ class benchmark_runner:
         return git_commit, has_changed_files
 
     def _execute_benchmark(self, benchmark_name):
-        exe_file_path = self._exe_full_path(benchmark_name)
+        exe_file_relative_path = self._exe_relative_path(benchmark_name)
         try:
-            with util.scoped_cd(self._install_dir):
-                _ = subprocess.check_output([exe_file_path, "-m", "b"])
+            with util.scoped_cd(self._binary_dir):
+                _ = subprocess.check_output([exe_file_relative_path, "-m", "b"])
             return True
-        except OSError:
+        except OSError as e:
             return False
 
+    def _exe_relative_path(self, benchmark_name: str):
+        return f"{benchmark_name}.exe"
+
     def _exe_full_path(self, benchmark_name):
-        exe_file_path = os.path.join(self._install_dir, benchmark_name + ".exe")
+        exe_file_path = os.path.join(self._binary_dir, _exe_relative_path(benchmark_name))
         return exe_file_path
 
     def _result_full_path(self, benchmark_name):
-        result_file_path = os.path.join(self._install_dir, benchmark_name + "_Profiling.json")
+        result_file_path = os.path.join(self._binary_dir, benchmark_name + "_Profiling.json")
         return result_file_path
 
     def run_single(self, benchmark_name, repeat_count):
@@ -63,6 +67,7 @@ class benchmark_runner:
         results = []
 
         for i in range(repeat_count):
+            diagnostic.report_info(f"Running benchmark <{benchmark_name}>, round {i+1} out of {repeat_count}.")
             if os.path.exists(result_file_path):
                 os.remove(result_file_path)
             success = self._execute_benchmark(benchmark_name)
@@ -70,9 +75,12 @@ class benchmark_runner:
                 raise NotImplementedError
             if not os.path.isfile(result_file_path):
                 raise NotImplementedError
+            diagnostic.report_info(f"Done. Collecting data ...")
+
             with open(result_file_path, encoding="utf-8") as result_file:
                 result = json.load(result_file)
                 results.append(result)
+            diagnostic.report_info("Collected.")
 
         return results
 
@@ -93,29 +101,43 @@ class benchmark_runner:
         BENCHMARK_DATABASE_PATH = os.path.join(self._root_dir, "doc", "contents", "materials", "benchmark.db.txt")
 
         start_time = datetime.datetime.now()
-
         git_commit, has_changed_files = self._get_git_commit()
-        if has_changed_files and len(changes_from_head) == 0:
-            print("Files were changed from last commit. Should have detailed description about new changes.")
-
-        results = {
-            benchmark_name: self.run_single(benchmark_name, REPEAT_COUNT)
-            for benchmark_name in BENCHMARKS
-        }
-
         cpu_info = cpuinfo.get_cpu_info()
+        if has_changed_files and len(changes_from_head) == 0:
+            diagnostic.report_error(
+                "Files were changed from last commit. Should have detailed description about new changes."
+                )
+
         task_result = \
             {
                 "date_time": start_time,
-                "end_time": datetime.datetime.now(),
+                "end_time": None,
                 "repeat_count": REPEAT_COUNT,
                 "node": platform.node(),
                 "cpu": cpu_info['brand'],
                 "os": platform.platform(),
                 "git_commit": git_commit,
                 "changes": changes_from_head,
-                "results": results
+                "results": None
             }
+
+        util.report_info()
+        diagnostic.report_info("Running info:")
+        for k, v in task_result.items:
+            if v is None or len(v) == 0:
+                continue
+            else:
+                diagnostic.report_info(" -{k}: {v}")
+        
+        results = {
+            benchmark_name: self.run_single(benchmark_name, REPEAT_COUNT)
+            for benchmark_name in BENCHMARKS
+        }
+
+        task_result.update({
+            "end_time": datetime.datetime.now(),
+            "results": results
+        })
 
         result_one_line_json = json.dumps(task_result)
 
