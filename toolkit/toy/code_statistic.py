2d158f2c91986ebb2eeb31396c27030913aeaaf5
wuye9036 <wuye9036@gmail.com>
2022-12-31 14:29:57 -0800

    Fix issues when opening folder by Visual Studio.
    Fixed CMake loading issue when using Visual Studio.

--- toolkit/toy/code_statistic.py
+++ toolkit/toy/code_statistic.py
@@ -1,71 +1,120 @@
-from __future__ import print_function
-import sys, os
-import os.path
+import sys
+import pathlib
+import dataclasses
 
-def stat_file( file_name, stat, total_file_size ):
-	total_file_size[0] += os.path.getsize(file_name)
-	f = open( file_name, "r" )
-	lines = f.readlines()
-	is_multiline_comment = False
-	for l in lines:
-		stripped_l = l.strip()
-		if not is_multiline_comment:
-			if stripped_l.startswith( "//" ):
-				stat["single line comments"] += 1
-				continue
-			if stripped_l.find("/*") != -1:
-				is_multiline_comment = True
-				if stripped_l[:2] == "/*" != -1:
-					stat["multi line comments"] += 1
-				else:
-					stat["mixed lines"] += 1
-			else:
-				if stripped_l == "":
-					stat["null lines"] += 1
-				elif stripped_l == "{" or stripped_l == "}":
-					stat["empty lines"] += 1
-				else:
-					stat["code lines"] += 1
-		else:
-			if stripped_l.find("*/") != -1:
-				is_multiline_comment = False
-				if stripped_l[-2:] == "*/":
-					stat["multi line comments"] += 1
-				else:
-					stat["mixed lines"] += 1
-			else:
-				stat["multi line comments"] += 1
-	f.close()
-	return file_name
-	
-def stat_dir( dir, ext_filters ):
-	stat = {"null lines":0, "empty lines":0, "single line comments":0, "multi line comments":0, "code lines":0, "mixed lines":0 }
-	total_file_size = [0]
-	processed_files = []
-	for root, subdirs, files in os.walk( dir ):
-		filterated_files = [ name for name in files if os.path.splitext(name)[1] in ext_filters ]
-		processed_files += [ stat_file(os.path.join(root, name), stat, total_file_size) for name in filterated_files ]
-	total = sum( stat.values() )
-	stat["total"] = total
-	stat["files"] = len( processed_files )
-	stat["file bytes(KB)"] = round(total_file_size[0] / 1024.0, 2)
-	stat["bytes per line"] = round(total_file_size[0] / float(total), 2)
-	return stat
-	
+@dataclasses.dataclass
+class Options:
+  IncludeList = []
+  ExcludeList = set()
+  ExtensionFilter = set()
+
+@dataclasses.dataclass(repr=True)
+class StatResult:
+  FileCount: int = 0
+  FileSizeTotal: int = 0
+  FileLines: int = 0
+
+  NullLines: int = 0
+  EmptyLines: int = 0
+  SingleLineComments: int = 0
+  MultiLineComments: int = 0
+  PreprocessorLines: int = 0
+  CodeLines: int = 0
+  MixedLines: int = 0
+
+def IsEmptyLine(l: str):
+  return not any(ch.isalpha() or ch.isdigit() for ch in l)
+
+def ParseFile(fPath: pathlib.Path, result: StatResult):
+  result.FileCount += 1
+  result.FileSizeTotal += fPath.stat().st_size
+
+  lines = fPath.read_text(encoding="utf-8").splitlines()
+  
+  in_multiline_comment = False
+  for l in lines:
+    stripped_l = l.strip()
+    if not in_multiline_comment:
+      if stripped_l.startswith("#"):
+        result.PreprocessorLines += 1
+        continue
+      if stripped_l.startswith( "//" ):
+        result.SingleLineComments += 1
+        continue
+      if stripped_l.find("/*") != -1:
+        in_multiline_comment = True
+        if stripped_l[:2] == "/*" != -1:
+          result.MultiLineComments += 1
+        else:
+          result.MixedLines += 1
+      else:
+        if stripped_l == "":
+          result.NullLines += 1
+        elif IsEmptyLine(stripped_l):
+          result.EmptyLines += 1
+        else:
+          result.CodeLines += 1
+    else:
+      if stripped_l.find("*/") != -1:
+        in_multiline_comment = False
+        if stripped_l[-2:] == "*/":
+          result.MultiLineComments += 1
+        else:
+          result.MixedLines += 1
+      else:
+        result.MultiLineComments += 1
+  
+def Scan(path: pathlib.Path, opt: Options, result: StatResult):
+  for child in path.iterdir():
+    assert isinstance(child, pathlib.Path)
+
+    child_s = child.resolve().as_posix()
+    if child_s in opt.ExcludeList:
+      # print(f"! Folder {child.resolve()} was excluded.")
+      continue
+
+    if child.is_dir():  
+      Scan(child, opt, result)
+    else:
+      if child.suffix in opt.ExtensionFilter:
+        ParseFile(child, result)
+
+def main():
+  root = pathlib.Path(".").resolve()
+  print(f"Root folder: {root}")
+
+  opt = Options()
+
+  opt.IncludeList = [
+    root / "salvia",
+    root / "samples",
+    root / "sasl",
+    root / "eflib"
+  ]
+
+  opt.ExcludeList = set(
+    p.resolve().as_posix() for p in [
+      root / "eflib" / "include" / "eflib" / "math" / "write_mask",
+      root / "eflib" / "include" / "eflib" / "math" / "swizzle",
+      root / "eflib" / "include" / "eflib" / "concurrency" / "thread_pool",
+      root / "sasl" / "include" / "sasl" / "enums",
+      root / "sasl" / "src" / "enums"
+    ]
+  )
+  opt.ExtensionFilter = set(
+    ".h;.cpp".split(";")
+  )
+
+  result = StatResult()
+
+  for fd in opt.IncludeList:
+    # result = StatResult()
+    # print(f">>> {fd.resolve().as_posix()}")
+    Scan(fd, opt, result)
+    # print(f"<<< {result}")
+  
+  print(f"{result}")
+  
+        
 if __name__ == "__main__":
-	if len(sys.argv) < 2:
-		print("error: paramters are not enough.")
-		print("use this script as:")
-		print("  code_statistic.py dir_name [\".ext;.fuck\"]")
-	else:
-		if len(sys.argv) == 3:
-			ext = sys.argv[2]
-		else:
-			ext = ".h;.cpp"
-		print("statistic directory: %s" % sys.argv[1])
-		print("with extensions: %s" % ext)
-		print("result:")
-		stat = stat_dir( sys.argv[1], set(ext.split(";")) )
-		stat_keys = ["files", "code lines", "null lines", "empty lines", "single line comments", "multi line comments", "mixed lines", "total", "file bytes(KB)", "bytes per line"]
-		for key in stat_keys:
-			print("  %s: %s" % (key, stat[key]))
+  main()
