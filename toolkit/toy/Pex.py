ecd87d82a1d2d942a9c906c8ff0f0a8964489cd3
wuye9036 <wuye9036@gmail.com>
2023-02-19 12:22:39 -0800

    Pex supports pipe style.

--- toolkit/toy/Pex.py
+++ toolkit/toy/Pex.py
@@ -2,6 +2,57 @@ import threading
 import typing
 import logging
 
+def algo_as_continuation(f):
+  '''
+  Decorator. Added contiuation form to the execution algorithm. For e.g.
+  ```
+  @algo_as_continuation
+  def then(sender, fn):
+    pass
+  ```
+  could be used as:
+  `then(then(sch.schedule(), f0), f1)`
+  OR
+  `then(f1)(then(f0)(sch.schedule()))`
+  
+  Or more human-readable by operator overloading:
+  
+  ```
+  sch.schedule() | then(f0) | then(f1)
+  ```
+  '''
+  def _impl(*args, **kwargs):
+    if isinstance(args[0], Sender):
+      # algo(Sender, ...) -> Sender
+      return Sender(f(*args, **kwargs))
+    return PartialSender(f, *args, **kwargs)
+  return _impl
+  
+class PartialSender:
+  def __init__(self, f, *args, **kwargs) -> None:
+    self._f = f
+    self._args = args
+    self._kwargs = kwargs
+    
+  def apply(self, sender):
+    assert isinstance(sender, Sender)
+    return Sender(self._f(sender, *self._args, **self._kwargs))
+    
+class Sender:
+  def __init__(self, impl) -> None:
+    self._impl = impl
+    
+  def __getattr__(self, __name: str):
+    return getattr(self._impl, __name)
+    
+  def __call__(self, *args, **kwds):
+    self._impl(*args, **kwds)
+    
+  def __or__(self, partial_sender: PartialSender):
+    assert isinstance(partial_sender, PartialSender)
+    return partial_sender.apply(self)
+
+
 class indent_history:
   def __init__(self) -> None:
     self._indent = -1
@@ -76,7 +127,7 @@ class loop_scheduler:
     self._loop = loop
 
   def schedule(self):
-    return loop_scheduler_task(self._loop)
+    return Sender(loop_scheduler_task(self._loop))
     
 class manual_event_loop: # manual_event_loop in libunifex
   def __init__(self) -> None:
@@ -166,6 +217,7 @@ class then_receiver:
     assert isinstance(result, va_pack)
     result.invoke(self._receiver.set_value)
 
+@algo_as_continuation
 def then(sender, fn):
   return then_sender(sender, fn)
 
@@ -291,6 +343,18 @@ def testThenChain():
     )
     
     assert count[0] == 3
+    
+@trace_test
+def testThenPipe():
+  with single_thread_context.create() as context:
+    scheduler = context.get_scheduler()
+    
+    count = [0]
+    sync_wait(
+      scheduler.schedule() | then(inc_count(count, 1)) | then(inc_count(count, 2))
+    )
+    
+    assert count[0] == 3
 
 def testLetOp():
   pass
@@ -298,6 +362,7 @@ def testLetOp():
 def _main():
   testThen()
   testThenChain()
+  testThenPipe()
 
 if __name__ == "__main__":
   _main()
