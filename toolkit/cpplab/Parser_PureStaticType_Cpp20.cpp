0068fb564e5d801bb58a9b4482126d8217fc3806
whoami <whoami@gmail.com>
2023-03-27 11:42:28 -0700

    CppLab: Refactored PureStaticType parser.

--- toolkit/cpplab/Parser_PureStaticType_Cpp20.cpp
+++ toolkit/cpplab/Parser_PureStaticType_Cpp20.cpp
@@ -8,93 +8,27 @@
 
 #include <fmt/core.h>
 
-// Utilities
-struct nil {};
+#include "Grammar.h"
+#include "Meta.h"
 
-template <typename T, template <typename...> class Tmpl>
-struct is_specialized_impl : std::false_type {};
-template <template <typename...> class Tmpl, typename... Ps>
-struct is_specialized_impl<Tmpl<Ps...>, Tmpl> : std::true_type {};
-
-template <typename T, template <typename...> class Tmpl>
-concept is_specialized = is_specialized_impl<T, Tmpl>::value;
-
-template <typename T>
-concept is_optional = is_specialized<T, std::optional>;
-
-template <typename T1, typename T2> struct combine_composition {};
-
-template <template <typename...> class C, typename... T1s, typename... T2s>
-struct combine_composition<C<T1s...>, C<T2s...>> {
-  using type = C<T1s..., T2s...>;
-};
-
-template <template <typename...> class C, typename... Ts>
-struct combine_composition<C<nil>, C<Ts...>> {
-  using type = C<Ts...>;
-};
-
-template <typename T1, typename T2>
-using combine_composible_t = typename combine_composition<T1, T2>::type;
-
-template <typename U, typename T> decltype(auto) cast_ref(T &v) { return ((U &)v); }
-
-template <typename U, typename T> decltype(auto) cast_ref(T const &v) { return ((U const &)v); }
-
-template <typename U, typename T> decltype(auto) cast_ref(T &&v) { return ((U &&) v); }
-
-namespace parsers {
-// Combinators
-template <typename... Ps> struct seq {};
-template <typename P> struct rep {};
-template <typename... Ps> struct br {};
-template <typename P> struct indirect_ {};
-
-// Rule declarations
-struct nomad {
-  static constexpr auto name = "nomad";
-};
-struct expr_mono {
-  static constexpr auto name = "expr_mono";
-};
-struct expr {
-  static constexpr auto name = "expr";
-};
-struct lit_int {
-  static constexpr auto name = "lit_int";
-};
-struct op {
-  static constexpr auto name = "op";
-};
-
-// Rule definitions and concepts
-template <typename T> struct combinator {};
-
-template <> struct combinator<expr_mono> {
-  using type = seq<lit_int, op, lit_int>;
-};
-
-template <> struct combinator<expr> {
-  using type = br<seq<lit_int, op, indirect_<expr>>, lit_int>;
-};
-
-template <typename T> using combinator_t = typename combinator<std::remove_cvref_t<T>>::type;
-
-template <typename T>
-concept has_combinator = requires { combinator_t<T>{}; };
+namespace pure_static_parsers {
+using namespace grammars;
 
 // Parsed data structure and operations
-template <typename Tag, typename D> struct tree_node {
+template <typename Tag, typename D>
+struct tree_node {
   using tag = Tag;
   using data_type = D;
 
   std::string_view code_range;
   D data{};
 
-  template <typename Tag2> using retagged = tree_node<Tag2, D>;
+  template <typename Tag2>
+  using retagged = tree_node<Tag2, D>;
 };
 
-template <typename Tag, typename D> auto make_tree(std::string_view code_range, D &&data) {
+template <typename Tag, typename D>
+auto make_tree(std::string_view code_range, D&& data) {
   return tree_node<Tag, std::remove_cvref_t<D>>{.code_range = code_range, .data = ((D &&) data)};
 }
 
@@ -106,27 +40,28 @@ concept is_optional_tree_node = is_optional<T> && is_tree_node<typename T::value
 
 template <typename Tag2, typename T>
   requires is_optional_tree_node<std::remove_cvref_t<T>> decltype(auto)
-replace_tag(T &&t) {
+replace_tag(T&& t) {
   using data_type = std::remove_cvref_t<typename T::value_type::data_type>;
   return cast_ref<std::optional<tree_node<Tag2, data_type>>>(((T &&) t));
 }
 
-template <typename P> struct indirect_data;
+template <typename P>
+struct indirect_data;
 
 template <typename T>
   requires is_optional<std::remove_cvref_t<T>>
-auto make_or_forward_optional(T &&v) {
+auto make_or_forward_optional(T&& v) {
   return ((T &&) v);
 }
 
 template <typename T>
   requires(!is_optional<std::remove_cvref_t<T>>)
-auto make_or_forward_optional(T &&v) {
+auto make_or_forward_optional(T&& v) {
   return std::make_optional(((T &&) v));
 }
 
 template <typename FnT>
-auto conditional_(bool cond, FnT &&fn) -> decltype(make_or_forward_optional(((FnT &&) fn)())) {
+auto conditional_(bool cond, FnT&& fn) -> decltype(make_or_forward_optional(((FnT &&) fn)())) {
   if (!cond) {
     return std::nullopt;
   }
@@ -135,55 +70,74 @@ auto conditional_(bool cond, FnT &&fn) -> decltype(make_or_forward_optional(((Fn
 
 #define DEBUG_TREE 0
 #if DEBUG_TREE == 1
-template <typename T> void print_tree(T &&, std::string indent) {
+template <typename T>
+void print_tree(T&&, std::string indent) {
   fmt::print("{}<unknown_data: {}>", indent, boost::core::demangle(typeid(T).name()));
 }
 #endif
 
 // return deduced type to resolve 'auto' issue
-template <typename P> struct parse_ {};
+template <typename P>
+struct parse_ {};
 
 template <typename P>
   requires has_combinator<std::remove_cvref_t<P>>
 struct parse_<P> {
   using type = std::remove_cvref_t<decltype(replace_tag<P>(
-          typename parse_<combinator_t<std::remove_cvref_t<P>>>::type{}))>;
+      typename parse_<combinator_t<std::remove_cvref_t<P>>>::type{}))>;
 };
 
-template <typename P> struct parse_<indirect_<P>> {
+template <typename P>
+struct parse_<indirect_<P>> {
   using type = std::optional<tree_node<P, std::unique_ptr<indirect_data<P>>>>;
 };
 
-template <typename... Ps> struct parse_<seq<Ps...>> {
+template <typename... Ps>
+struct parse_<seq<Ps...>> {
   using type =
       std::optional<tree_node<nomad, std::tuple<typename parse_<Ps>::type::value_type...>>>;
 };
 
-template <typename... Ps> struct parse_<br<Ps...>> {
+template <typename... Ps>
+struct parse_<br<Ps...>> {
   using type =
       std::optional<tree_node<nomad, std::variant<typename parse_<Ps>::type::value_type...>>>;
 };
 
-template <> struct parse_<lit_int> {
+template <>
+struct parse_<lit_int> {
   using type = std::optional<tree_node<lit_int, int>>;
 };
 
-template <> struct parse_<op> {
+template <>
+struct parse_<op> {
   using type = std::optional<tree_node<op, char>>;
 };
 
-template <typename T> using parse_t = typename parse_<T>::type;
+template <typename T>
+using parse_t = typename parse_<T>::type;
 
 [[maybe_unused]] void _instantiate_parse_return_types() {
-    [[maybe_unused]] auto e = parse_t<expr>{};
+  [[maybe_unused]] auto e = parse_t<expr>{};
 }
 
 // #define FUCK_STUPID_COMPILER(deduced_type)
 #define FUCK_STUPID_COMPILER(deduced_type) deduced_type
 
+auto parse(std::string_view sv, lit_int) FUCK_STUPID_COMPILER(->parse_t<lit_int>);
+
+template <typename... Ps>
+auto parse(std::string_view sv, seq<Ps...>) FUCK_STUPID_COMPILER(->parse_t<seq<Ps...>>);
+
+template <typename... Ps>
+auto parse(std::string_view sv, br<Ps...>) FUCK_STUPID_COMPILER(->parse_t<br<Ps...>>);
+
+template <typename P>
+auto parse(std::string_view sv, indirect_<P>) FUCK_STUPID_COMPILER(->parse_t<indirect_<P>>);
+
 template <typename OptTree, typename Fn>
   requires is_optional_tree_node<std::remove_cvref_t<OptTree>>
-auto operator&(OptTree &&op1, Fn &&op2_factory) {
+auto operator&(OptTree&& op1, Fn&& op2_factory) {
   using tree_type = typename std::remove_cvref_t<OptTree>::value_type;
   using op1_data_type = typename tree_type::data_type;
   using op2_data_type = std::tuple<typename decltype(((Fn &&) op2_factory)())::value_type>;
@@ -196,14 +150,15 @@ auto operator&(OptTree &&op1, Fn &&op2_factory) {
       auto fold_code_range = std::string_view{op1->code_range.begin(), op2->code_range.end()};
       return std::make_optional(make_tree<typename tree_type::tag, return_data_type>(
           fold_code_range,
-          std::tuple_cat(((OptTree&&)op1).value().data, std::make_tuple(std::move(op2).value()))));
+          std::tuple_cat(
+              ((OptTree &&) op1).value().data, std::make_tuple(std::move(op2).value()))));
     });
   });
 }
 
 template <typename TreeA, typename Fn>
   requires is_optional_tree_node<std::remove_cvref_t<TreeA>>
-auto operator|(TreeA &&op1, Fn &&op2_factory) {
+auto operator|(TreeA&& op1, Fn&& op2_factory) {
   using op1_data_type = typename std::remove_cvref_t<TreeA>::value_type::data_type;
   using op2_data_type = std::variant<typename decltype(((Fn &&) op2_factory)())::value_type>;
 
@@ -214,10 +169,7 @@ auto operator|(TreeA &&op1, Fn &&op2_factory) {
   if constexpr (!std::is_same_v<op1_data_type, std::variant<nil>>) {
     if (op1.has_value()) {
       std::visit(
-          [&ret_data](auto &&v) {
-            ret_data = std::forward<decltype(v)>(v);
-          },
-          std::move(op1->data));
+          [&ret_data](auto&& v) { ret_data = std::forward<decltype(v)>(v); }, std::move(op1->data));
 
       return std::make_optional(make_tree<nomad>(op1->code_range, std::move(ret_data)));
     }
@@ -246,13 +198,14 @@ auto parse(std::string_view sv, op) FUCK_STUPID_COMPILER(->parse_t<op>) {
   });
 }
 
-template <has_combinator P> auto parse(std::string_view sv, P) FUCK_STUPID_COMPILER(->parse_t<P>) {
-  auto parsed = parse(sv, combinator_t<P>{});
+template <has_combinator P>
+auto parse(std::string_view sv, P) FUCK_STUPID_COMPILER(->parse_t<P>) {
+  auto parsed = parse(sv, grammars::combinator_t<P>{});
   return replace_tag<P>(std::move(parsed));
 }
 
 template <typename... Ps>
-auto parse(std::string_view sv, seq<Ps...>) FUCK_STUPID_COMPILER(->parse_t<seq<Ps...>>) {
+auto parse(std::string_view sv, grammars::seq<Ps...>) FUCK_STUPID_COMPILER(->parse_t<seq<Ps...>>) {
   auto t = std::tuple<Ps...>{};
   return std::apply(
       [&](auto... parsers) {
@@ -281,7 +234,8 @@ auto parse(std::string_view sv, br<Ps...>) FUCK_STUPID_COMPILER(->parse_t<br<Ps.
       t);
 }
 
-template <typename P> struct indirect_data {
+template <typename P>
+struct indirect_data {
   static_assert(has_combinator<P>);
 
   using parsed_type = parse_t<P>;
@@ -305,7 +259,7 @@ auto parse(std::string_view sv, indirect_<P>) FUCK_STUPID_COMPILER(->parse_t<ind
 // Debugging tree
 template <typename OptD>
   requires is_optional<std::remove_cvref_t<OptD>>
-void print_tree(OptD &&opt, std::string indent) {
+void print_tree(OptD&& opt, std::string indent) {
   if (!opt) {
     fmt::print("{}{}\n", indent, "<opt: null>");
     return;
@@ -316,25 +270,26 @@ void print_tree(OptD &&opt, std::string indent) {
 
 template <typename Tuple>
   requires is_specialized<std::remove_cvref_t<Tuple>, std::tuple>
-void print_tree(Tuple &&t, std::string indent) {
+void print_tree(Tuple&& t, std::string indent) {
   fmt::print("{}tuple:\n", indent);
   std::apply(
-      [&indent](auto &&...args) {
+      [&indent](auto&&... args) {
         (print_tree(std::forward<decltype(args)>(args), indent + "  "), ...);
       },
       t);
 }
 
 template <typename D>
-concept inline_printable_strict = (std::same_as<D, std::string_view> || std::integral<D> ||
-                                   std::floating_point<D> || std::same_as<D, std::string>);
+concept inline_printable_strict =
+    (std::same_as<D, std::string_view> || std::integral<D> || std::floating_point<D> ||
+     std::same_as<D, std::string>);
 
 template <typename D>
 concept inline_printable = inline_printable_strict<std::remove_cvref_t<D>>;
 
 template <typename T>
   requires is_tree_node<std::remove_cvref_t<T>>
-void print_tree(T &&t, std::string indent = "") {
+void print_tree(T&& t, std::string indent = "") {
   using tree_type = std::decay_t<T>;
   using data_type = typename tree_type::data_type;
   if constexpr (inline_printable<data_type>) {
@@ -347,35 +302,35 @@ void print_tree(T &&t, std::string indent = "") {
 
 template <typename T>
   requires is_specialized<std::remove_cvref_t<T>, std::variant>
-void print_tree(T &&t, std::string indent = "") {
+void print_tree(T&& t, std::string indent = "") {
   fmt::print("{}variant\n", indent);
-  std::visit([&](auto &&v) { print_tree(std::forward<decltype(v)>(v), indent + "  "); },
-             ((T &&) t));
+  std::visit(
+      [&](auto&& v) { print_tree(std::forward<decltype(v)>(v), indent + "  "); }, ((T &&) t));
 }
 
 template <typename T>
   requires is_specialized<std::remove_cvref_t<T>, indirect_data>
-void print_tree(T &&t, std::string indent = "") {
+void print_tree(T&& t, std::string indent = "") {
   fmt::print("{}indirect\n", indent);
   print_tree(((T &&) t).value, indent + "  ");
 }
 
 template <typename T>
   requires is_specialized<std::remove_cvref_t<T>, std::unique_ptr>
-void print_tree(T &&p, std::string indent = "") {
+void print_tree(T&& p, std::string indent = "") {
   fmt::print("{}unique_ptr\n", indent);
   print_tree((*p), indent + "  ");
 }
-} // namespace parsers
+}  // namespace pure_static_parsers
 
-using namespace parsers;
+using namespace pure_static_parsers;
 
 TEST(MetaParser, MetaTokenizer) {
   static_assert(std::is_same_v<combinator_t<expr_mono>, seq<lit_int, op, lit_int>>);
 
-  auto r1 = parsers::parse("1+2", expr_mono{});
+  auto r1 = pure_static_parsers::parse("1+2", grammars::expr_mono{});
   print_tree(r1, "");
 
-  auto r2 = parsers::parse("1+2+3", parsers::expr{});
+  auto r2 = pure_static_parsers::parse("1+2+3", grammars::expr{});
   print_tree(r2, "");
 }
--- toolkit/cpplab/Parser_SemiStaticType_Cpp20.cpp
+++ toolkit/cpplab/Parser_SemiStaticType_Cpp20.cpp
@@ -1,91 +1,84 @@
 #include <gtest/gtest.h>
 
-#include <type_traits>
 #include <memory>
+#include <type_traits>
 
 class attribute {
 public:
-    template <typename T>
-    attribute& operator = (T&) {}
+  template <typename T>
+  attribute& operator=(T&) {}
 
-    template <typename T>
-    attribute& operator = (T&&) {}
+  template <typename T>
+  attribute& operator=(T&&) {}
 
-    void accept() {
-        attr_impl_->accept();
-    }
+  void accept() { attr_impl_->accept(); }
 
-    struct _concept {
-        virtual void accept() = 0;
-        virtual ~_concept() {}
-    };
+  struct _concept {
+    virtual void accept() = 0;
+    virtual ~_concept() = default;
+  };
 
-    template <typename Visitor, typename Rule, typename AttrValue>
-    struct _model final: _concept {
-        template<typename AttrValue2>
-        _model(Visitor& visitor, AttrValue2&& value): visitor(visitor), attr_value{((AttrValue2&&)value)} {
-        }
-        void accept() override {
-            visitor(Rule{}, attr_value);
-        }
-        Visitor& visitor;
-        AttrValue attr_value;
-    };
+  template <typename Visitor, typename Rule, typename AttrValue>
+  struct _model final : _concept {
+    template <typename AttrValue2>
+    _model(Visitor& visitor, AttrValue2&& value)
+      : visitor(visitor)
+      , attr_value{((AttrValue2 &&) value)} {}
+    void accept() override { visitor(Rule{}, attr_value); }
+    Visitor& visitor;
+    AttrValue attr_value;
+  };
 
-    template <typename Visitor, typename Rule, typename AttrValue>
-    attribute(Visitor& visitor, Rule, AttrValue&& value): attr_impl_{
-        std::make_unique<_model<Visitor, Rule, AttrValue>>(visitor, (AttrValue&&)value)}
-    {
-    }
+  template <typename Visitor, typename Rule, typename AttrValue>
+  attribute(Visitor& visitor, Rule, AttrValue&& value)
+    : attr_impl_{
+          std::make_unique<_model<Visitor, Rule, AttrValue>>(visitor, (AttrValue &&) value)} {}
 
 private:
-    std::unique_ptr<_concept> attr_impl_;
+  std::unique_ptr<_concept> attr_impl_;
 };
 
 class value_of_seq {
-    std::vector<attribute> children;
+  std::vector<attribute> children;
 };
 
 class value_of_queue {
-    std::vector<attribute> children;
+  std::vector<attribute> children;
 };
 
 class value_of_select {
-    std::unique_ptr<attribute> selected;
+  std::unique_ptr<attribute> selected;
 };
 
-class value_of_term {
-};
+class value_of_term {};
 
 struct rules {
-    struct expr{};
+  struct expr {};
 };
 
 template <typename... Ts>
-struct overloads: Ts... {
-    using Ts::operator() ...;
+struct overloads : Ts... {
+  using Ts::operator()...;
 };
 
-template <typename... Ts> overloads(Ts&&...) -> overloads<Ts...>;
+template <typename... Ts>
+overloads(Ts&&...) -> overloads<Ts...>;
 
 template <typename Visitor, typename Grammar, typename TokenRange>
 attribute parse(Visitor& visitor, Grammar const& g, TokenRange tokens) {
-    return {visitor, rules::expr{}, value_of_term{}};
+  return {visitor, rules::expr{}, value_of_term{}};
 }
 
-struct grammar{};
+struct grammar {};
 
 struct token_range {};
 
-TEST(SemiStaticAttribute, SemiStaticAttribute){
-    auto visitor = overloads {
-        [] (auto r, auto&& v){
-            static_assert(std::is_same_v<rules::expr, std::remove_cvref_t<decltype(r)>>);
-        }
-    };
+TEST(SemiStaticAttribute, SemiStaticAttribute) {
+  auto visitor = overloads{[](auto r, auto&& v) {
+    static_assert(std::is_same_v<rules::expr, std::remove_cvref_t<decltype(r)>>);
+  }};
 
-    auto attr = parse(visitor, grammar{}, token_range{});
+  auto attr = parse(visitor, grammar{}, token_range{});
 
-    attr.accept();
+  attr.accept();
 }
-
